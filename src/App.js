95aabc9a17b88d4f0c62fc530b8ac15044651332
naavika <niksshahiruchi@gmail.com>
2024-08-26 21:07:52 +0530

    Understanding deeper concepts of React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import Button from './components/UI/Button/Button';
+import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowparagraph) => !prevShowparagraph);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput/>
+      <Button onClick = {toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
